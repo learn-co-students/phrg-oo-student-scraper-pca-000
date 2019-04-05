d08c489e5e54dd958daee96e48562ba65dd9e9ce
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-04-05 15:22:09 -0400

    able to get all the student tests to pass, just need to understand scraping.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,16 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(url)
+    info = Nokogiri::HTML(open(url))
+    names = info.css('.student-name').text.split
+    students = []
+
+# binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
